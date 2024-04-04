ce9420cf1b12153769f868fae9ff58676885dee5
D <56808335+enamchae@users.noreply.github.com>
2024-04-04 22:01:09 +0000

    fix: fix bug 3 (reload all employees)

--- src/App.tsx
+++ src/App.tsx
@@ -31,10 +31,13 @@ export function App() {
 
   const loadTransactionsByEmployee = useCallback(
     async (employeeId: string) => {
+      if (employeeId === EMPTY_EMPLOYEE.id) {
+        return await loadAllTransactions()
+      }
       paginatedTransactionsUtils.invalidateData()
       await transactionsByEmployeeUtils.fetchById(employeeId)
     },
-    [paginatedTransactionsUtils, transactionsByEmployeeUtils]
+    [paginatedTransactionsUtils, transactionsByEmployeeUtils, loadAllTransactions]
   )
 
   useEffect(() => {
